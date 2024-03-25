652cc703f0785a1f9fe663415d7db4534bcbd4a0
Łukasz <kaczor.lukasz@icloud.com>
2024-03-25 20:36:46 +0100

    Updated work experience component

--- CvCreator/src/app/Components/Creator/work-experience/work-experience.component.ts
+++ CvCreator/src/app/Components/Creator/work-experience/work-experience.component.ts
@@ -1,5 +1,5 @@
 import { Component, Input, OnChanges, OnInit } from '@angular/core';
-import { FormBuilder } from '@angular/forms';
+import { FormBuilder, FormControl, Validators } from '@angular/forms';
 import { WorkExperience } from 'src/app/Models/WorkExperience';
 import { CvAddressService } from 'src/app/Services/cv-address.service';
 import { CvExperienceService } from 'src/app/Services/cv-experience.service';
@@ -8,7 +8,7 @@ import { FormManager } from 'src/app/Utilities/FormManager';
 @Component({
   selector: 'app-work-experience',
   templateUrl: './work-experience.component.html',
-  styleUrls: ['./work-experience.component.scss']
+  styleUrls: ['../../../../Styles/forms.scss'],
 })
 export class WorkExperienceComponent
 extends FormManager<WorkExperience>
@@ -19,28 +19,19 @@ implements OnInit, OnChanges {
 
   constructor(service: CvExperienceService ,builder: FormBuilder) {
     super(service, builder)
-   }
+  }
 
-  //  id: string;
-  //  name: string;
-  //  companyName: string;
-  //  city: string;
-  //  startDate: Date;
-  //  endDate: Date;
-  //  stillEmployed: boolean;
-  //  description: string;
-  //  curriculumVitaeId: string;
 
   ngOnInit(): void {
     this.initializeForm({
       id: [''],
-      name: [''],
-      companyName: [''],
-      city: [''],
-      startDate: [''],
+      name: ['', [Validators.required, Validators.minLength(2), Validators.maxLength(64)]],
+      companyName: ['', [Validators.required, Validators.minLength(2), Validators.maxLength(64)]],
+      city: ['', [Validators.required, Validators.minLength(2), Validators.maxLength(32)]],
+      startDate: ['', [Validators.required]],
       endDate: [''],
       stillEmployed: [''],
-      description: [''],
+      description: ['',  [Validators.maxLength(128)]],
       curriculumVitaeId: [this.curriculumVitaeId],
     });
   }
@@ -49,5 +40,54 @@ implements OnInit, OnChanges {
   ngOnChanges() {
     if (!this.experience) return;
     // this.initFormData(this.experience);
+  }
+
+  get name(): FormControl {
+    return this.form.get('name') as FormControl;
+  }
+  set name(val) {
+    this.name?.setValue(val);
+  }
+
+  get companyName(): FormControl {
+    return this.form.get('companyName') as FormControl;
+  }
+  set companyName(val) {
+    this.companyName?.setValue(val);
+  }
+
+  get city(): FormControl {
+    return this.form.get('city') as FormControl;
+  }
+  set city(val) {
+    this.city?.setValue(val);
+  }
+
+  get startDate(): FormControl {
+    return this.form.get('startDate') as FormControl;
+  }
+  set startDate(val) {
+    this.startDate?.setValue(val);
+  }
+
+  get endDate(): FormControl {
+    return this.form.get('endDate') as FormControl;
+  }
+  set endDate(val) {
+    this.endDate?.setValue(val);
+  }
+
+  get stillEmployed(): FormControl {
+    return this.form.get('stillEmployed') as FormControl;
+  }
+  set stillEmployed(val) {
+    this.stillEmployed?.setValue(val);
+  }
+
+  get description(): FormControl {
+    return this.form.get('description') as FormControl;
+  }
+  set description(val) {
+    this.description?.setValue(val);
   }
 }
